2f2e28ab5ac9aa48a224a3828a35be045bb280a2
Yifeng <45142783+huang1997214@users.noreply.github.com>
2021-03-16 02:36:52 +0800

    Add files via upload

--- Test.py
+++ Test.py
@@ -1,58 +1,91 @@
 from DNN_model import Get_ResNet_50, Get_VGG_16
-from utils import vertex_extract, open_image, process_list, load_data
+from utils import vertex_extract, open_image, Layer, VM, evaluate_performance
+import cv2
+import cnn_finetune
 import torch
-import datetime
-import ray
-import gc
-import psutil
-
-def auto_garbage_collect(pct=80.0):
-    if psutil.virtual_memory().percent >= pct:
-        gc.collect()
-
-#Build the model
-VGG_16 = Get_VGG_16()
-VGG_16 = VGG_16
-#Extract Layers
-layer_list = vertex_extract(VGG_16)
-print(layer_list)
-'''
-#Divide Virtual Machine
-divid_index = 19
-VM_1 = layer_list[0:divid_index]
-VM_2 = layer_list[divid_index:len(layer_list)]
-#
-image_list = load_data()
-starttime = datetime.datetime.now()
-for img in image_list:
-    img = torch.unsqueeze(img, 0)
-    com_result = process_list(VM_1, img)
-    res = process_list(VM_2, com_result)
-    #res = VGG_16(img)
-endtime = datetime.datetime.now()
-running_time = endtime - starttime
-print('Original Running Time', running_time)
-ray.init()
-
-@ray.remote
-def process_VM_1(x):
-    print('xxxxxxxxxxxx')
-    return process_list(VM_1, x)
-
-@ray.remote
-def process_VM_2(x):
-    print('aaaaaaaaaaaa')
-    return process_list(VM_2, x)
-
-starttime = datetime.datetime.now()
-img = image_list[0]
+import numpy as np
+img = open_image('1.jpg')
 img = torch.unsqueeze(img, 0)
-com_result = ray.get(process_VM_1.remote(img))
-auto_garbage_collect()
-res = ray.get(process_VM_2.remote(com_result))
-auto_garbage_collect()
-ray.shutdown()
-endtime = datetime.datetime.now()
-running_time = endtime - starttime
-print('2VM Running Time', running_time)
-'''
+VGG_16 = Get_VGG_16()
+res = vertex_extract(VGG_16)
+#Divide Index
+divid_index = 19
+
+index = 0
+layer_list = []
+for layer in res:
+    if isinstance(layer, torch.nn.modules.conv.Conv2d):
+        temp_class = Layer('conv', img.shape, layer(img).shape, index, layer.kernel_size[0],
+                           layer.stride[0], index - 1)
+        img = layer(img)
+    elif isinstance(layer, torch.nn.modules.activation.ReLU):
+        temp_class = Layer('ReLu', img.shape, layer(img).shape, index, 0,
+                           0, index - 1)
+        img = layer(img)
+
+    elif isinstance(layer, torch.nn.modules.pooling.MaxPool2d):
+        temp_class = Layer('pool', img.shape, layer(img).shape, index, layer.kernel_size,
+                           layer.stride, index - 1)
+        img = layer(img)
+    elif isinstance(layer, torch.nn.modules.dropout.Dropout):
+        temp_class = Layer('Dropout', img.shape, layer(img).shape, index, 0,
+                           0, index - 1)
+        img = layer(img)
+    elif isinstance(layer, torch.nn.modules.linear.Linear):
+        img = img.view(-1)
+        temp_class = Layer('FC', img.shape, layer(img).shape, index, 0,
+                           0, index - 1)
+
+        img = layer(img)
+    layer_list.append(temp_class)
+    index += 1
+
+vm1 = VM(1e5, 8e8, 1)
+vm2 = VM(1e5, 6e8, 2)
+vm3 = VM(1e5, 6e8, 3)
+vm_list = [vm1, vm2, vm3]
+
+for layer in layer_list:
+    assigned = False
+    if layer.dependence != -1:
+        for vm in vm_list:
+            if vm.check_dependence(layer):
+                if vm.cal_distance(layer) != -1:
+                    vm.assign_layer(layer)
+                    assigned = True
+    if not assigned:
+        dis_list = []
+        for vm in vm_list:
+            dis_list.append(vm.cal_distance(layer))
+        dis = max(dis_list)
+        dis_max = np.argmax(np.array(dis_list))
+        if dis == -1:
+            print('Failed!!!!!')
+        vm_list[dis_max].assign_layer(layer)
+#Our policy Result
+for i in range(len(vm_list)):
+    print(vm_list[i].assign_layer_id)
+#Evaluation
+running_time, communication = evaluate_performance(vm_list, layer_list)
+print(running_time)
+print(communication)
+
+#Random policy Result
+#init VM
+vm1 = VM(1e5, 8e8, 1)
+vm2 = VM(1e5, 6e8, 2)
+vm3 = VM(1e5, 6e8, 3)
+vm_list = [vm1, vm2, vm3]
+for layer in layer_list:
+    assigned = False
+    while not assigned:
+        vm_id = np.random.randint(0,3)
+        if vm_list[vm_id].cal_distance(layer) != -1:
+            vm_list[vm_id].assign_layer(layer)
+            assigned = True
+for i in range(len(vm_list)):
+    print(vm_list[i].assign_layer_id)
+#Evaluation
+running_time, communication = evaluate_performance(vm_list, layer_list)
+print(running_time)
+print(communication)
--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import torchvision
 import torch
 from PIL import Image
 from torchvision import transforms
-import os
+import numpy as np
 
 def vertex_extract(model):
     '''
@@ -11,7 +11,6 @@ def vertex_extract(model):
     :return: layer list
     '''
     layer_list = []
-    index = 0
     for layer in model.named_modules():
         #Skip Same Special Layers in the cnn_finetune
         if isinstance(layer[1], cnn_finetune.contrib.torchvision.ResNetWrapper):
@@ -23,8 +22,7 @@ def vertex_extract(model):
         if isinstance(layer[1], cnn_finetune.contrib.torchvision.VGGWrapper):
             continue
         else:
-            layer_list.append([index, layer[1]])
-            index += 1
+            layer_list.append(layer[1])
     return layer_list
 
 def Generate_DAG_Graph(layer_list, model_name):
@@ -40,21 +38,88 @@ def open_image(img_dir, img_resize=224):
     img = template_transform(img)
     return img
 
-def load_data():
-    path = os.path.join(os.getcwd(), 'query_image')
-    image_dir_list = os.listdir(path)
-    for i in range(len(image_dir_list)):
-        image_dir_list[i] = os.path.join(path, image_dir_list[i])
-    image_dir_list = image_dir_list[0:1]
-    image_list = []
-    for dir in image_dir_list:
-        image_list.append(open_image(dir))
-    return image_list
+drop_out_rate = 3
+Relu_para = 4
 
-def process_list(list_layer, x):
-    for _layer in list_layer:
-        index, layer = _layer
-        x = layer(x)
-        if index == 30:
-            x = x.view(x.size(0), -1)
-    return x
+class Layer():
+    def __init__(self, layer_type, in_shape, out_shape, index, kernel_size, stride, dependence):
+        self.layer_type = layer_type
+        self.in_shape = in_shape
+        self.out_shape = out_shape
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.index = index
+        self.dependence = dependence
+        assert self.layer_type in ['conv', 'pool', 'ReLu', 'Dropout', 'FC']
+        if self.layer_type == 'ReLu':
+            if len(in_shape)==4:
+                self.memory = 2 * in_shape[0] * in_shape[1] * in_shape[2] * in_shape[3] * 16
+                self.cpu = Relu_para * in_shape[0] * in_shape[1] * in_shape[2]
+                self.in_size = in_shape[1] * in_shape[2] * in_shape[3]
+            else:
+                self.memory = 2 * in_shape[0] * 16
+                self.cpu = Relu_para * in_shape[0]
+                self.in_size = in_shape[0]
+        elif self.layer_type == 'FC':
+            self.memory = (in_shape[0] + out_shape[0]) * 16
+            self.cpu = 2 * in_shape[0] * out_shape[0]
+            self.in_size = in_shape[0]
+        elif self.layer_type == 'Dropout':
+            self.memory = (in_shape[0] + out_shape[0]) * 16
+            self.cpu = drop_out_rate * in_shape[0] * out_shape[0] + (in_shape[0] + out_shape[0])
+            self.in_size = in_shape[0]
+        elif self.layer_type == 'conv':
+            self.memory = 2 * in_shape[1] * in_shape[2] * in_shape[3] * 16 \
+                          + 2 * out_shape[1] * out_shape[2] * out_shape[3] * 16
+            self.cpu = in_shape[1] * in_shape[2] * in_shape[3] * kernel_size**2 / (stride**2)
+            self.in_size = in_shape[1] * in_shape[2] * in_shape[3]
+        elif self.layer_type == 'pool':
+            self.memory = 2 * in_shape[1] * in_shape[2] * in_shape[3] * 16 \
+                          + 2 * out_shape[1] * out_shape[2] * out_shape[3] * 16
+            self.cpu = in_shape[1] * in_shape[2] * in_shape[3] * kernel_size ** 2 / (stride ** 2)
+            self.in_size = in_shape[1] * in_shape[2] * in_shape[3]
+
+class VM():
+    def __init__(self, cpu, memory, id):
+        self.cpu = cpu
+        self.memory = memory
+        self.id = id
+        self.assign_layer_id = []
+        self.memory_weight = 1
+        self.cpu_weight = 1
+        self.cpu_cal = 0
+
+    def assign_layer(self, layer):
+        self.assign_layer_id.append(layer.index)
+        self.cpu_cal += layer.cpu
+        self.memory -= layer.memory
+
+    def check_dependence(self, layer):
+        if layer.dependence in self.assign_layer_id:
+            return True
+        else:
+            return False
+
+    def cal_distance(self, layer):
+        if self.memory - layer.memory<0:
+            return -1
+        distance = self.memory_weight * (self.memory - layer.memory) \
+                   + self.cpu_cal
+        return distance
+
+def evaluate_performance(vm_list, layer_list):
+    bandwidth = 10000
+    communication = 0
+    running_time = 0
+    for layer in layer_list:
+        index = layer.index
+        for vm in vm_list:
+            if index in vm.assign_layer_id:
+                #If need communication
+                if index != 0:
+                    if layer.dependence not in vm.assign_layer_id:
+                        communication += layer.in_size
+                        running_time += layer.in_size / bandwidth
+                #Running
+                running_time += layer.cpu / (vm.cpu * 1000)
+    return running_time, communication
